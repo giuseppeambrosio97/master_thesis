51fa116a9f94371faf8cd9835a2c7076a8723ae2
Peppe <peppe@pop-os.localdomain>
2021-12-04 09:28:16 +0100

    preprocessing based on choice_deleted_edge_greedy

---
+++ src/clustering_deletion_edge_contraction_based/preprocessing_deleted_edge.py
@@ -0,0 +1,12 @@
+from src.clustering_deletion_edge_contraction_based.choice import choice_deleted_edge_greedy_value
+from src.clustering_deletion_edge_contraction_based.clustering_deletion_edge_contraction import edge_contraction
+
+
+def preprocess(G):
+    c = 0
+    while True:
+        e, value = choice_deleted_edge_greedy_value(G, list(G.edges))
+        if(value > 0):
+            return G, c
+        edge_contraction(G, e)
+        c += 1
